9601d811525070392973433da352c9c5c52935ed
not-the <87151784+not-the@users.noreply.github.com>
2023-11-29 23:03:21 -0500

    v2

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // PIXI.js Setup
-let app = new PIXI.Application({ width: 1200, height: 672 });
+const elGame = document.getElementById('game');
+// let app = new PIXI.Application({ width: 1200, height: 672 });
+let app = new PIXI.Application({ resizeTo:elGame });
 app.renderer.background.color = 0x111111;
 PIXI.BaseTexture.defaultOptions.scaleMode = PIXI.SCALE_MODES.NEAREST;
-document.getElementById('game').appendChild(app.view);
+
+elGame.appendChild(app.view);
+
+/** Returns an angle in radians given two objects */
+function angleRelative(one, two) { return Math.atan2(one.x-two.x, one.y-two.y); }
 
 let mouse = {
     s: {
@@ -12,12 +18,99 @@ let mouse = {
     mass: 1,
     grav_law: 1.01,
 }
+let elapsed = 0.0;
+let delta = 1;
+
+let user = {
+    dragging: undefined,
+}
+
+const inTrails = document.getElementById('trails');
+let config = {
+    pause: false,
+    game_speed: 1,
+
+    planet_size: 25,
+    planetGravityMultiplier: 0,
+    trails: true,
+    do_planet_collision: true,
+
+    worldGravity: 0.5,
+    wallBounce: 0.6,
+    edgeBehavior: 'collide',
+    airFriction: 0.01,
+    // wallFriction: 2,
+
+    rope_length: 100,
+    stiff_rope: false,
+
+    ...JSON.parse(store('gravity_toy_config')) ?? {}
+}
+document.querySelectorAll('[data-config]').forEach(element => {
+    let vName = element.type === 'checkbox' ? 'checked' : 'value';
+    element[vName] = config[element.id];
+    element.addEventListener('change', event => {
+        let value = element[vName];
+        if(element.type === 'number') value = Number(value);
+        config[element.id] = value;
+        if(store('main_cookies') === 'true') store('gravity_toy_config', JSON.stringify(config));
+    })
+})
+
+// Brushes
+let brush = 'planet';
+document.querySelectorAll('[data-brush]').forEach(element => {
+    if(element.dataset.brush === brush) element.classList.add('active');
+    element.addEventListener('click', event => {
+        document.querySelectorAll('.active[data-brush]').forEach(rel => rel.classList.remove('active'));
+        brush = event.srcElement.dataset.brush;
+        event.srcElement.classList.add('active');
+    })
+})
+
+/** Hypotenuese, returns distance between 2 points */
+function hypot(one, two) {
+    let distX = one.x - two.x;
+    let distY = one.y - two.y;
+    return [Math.sqrt(distX**2 + distY**2), distX, distY];
+}
+/** Hypotenuese, returns distance between the centers of 2 objects */
+function hypotCenter(one, two) {
+    let distX = (one.x+one.width/2) - (two.x+two.width/2);
+    let distY = (one.y+one.width/2) - (two.y+two.width/2);
+    return [Math.sqrt(distX**2 + distY**2), distX, distY];
+}
+
+function percentage(partialValue, totalValue) { return (100 * partialValue) / totalValue; }
+
+
+const container = new PIXI.Container();
+container.pivot.x = app.view.width/2;
+container.pivot.y = app.view.height/2;
+container.x = app.view.width/2;
+container.y = app.view.height/2;
+
+const trailsContainer = new PIXI.Container();
+container.addChild(trailsContainer);
+// trailsContainer.zIndex = 0;
+
+const planetContainer = new PIXI.Container()
+container.addChild(planetContainer);
+// planetContainer.zIndex = 1;
+
+
+
+app.stage.addChild(container);
+
+const in_planet_size = document.getElementById('planet_size');
 
 var pressed = {}
 var entities = {}
 var entityID = 0;
+var particles = {}
+var particleID = 0;
 class Planet {
-    constructor(mass=0.05, x=500, y=500, color=0xffffff) {
+    constructor(mass=in_planet_size.value/200, x=500, y=500, color=0xffffff) {
         // Prep
         let size = mass*200; // Size and mass are the same number for now
 
@@ -25,10 +118,10 @@ class Planet {
         this.s = new PIXI.Graphics();
         this.s.beginFill(color);
 
-        this.s.drawCircle(size, size, size);
-        this.s.endFill();
+        this.s.drawCircle(size, size, size).endFill();
         this.s.x = x;
         this.s.y = y;
+        this.s.zIndex = entityID;
         this.id = entityID;
         
         this.grav_law = 1.01;
@@ -36,56 +129,205 @@ class Planet {
         // Game
         this.mass = mass;
         this.motion = { x: 0, y: 0, r: 0 };
-        app.stage.addChild(this.s);
+        // app.stage.addChild(this.s);
+        planetContainer.addChild(this.s);
+
+        // Clickable
+        this.s.eventMode = 'static';
+        this.s.buttonMode = true;
+        this.s.cursor = 'pointer';
+        this.s.on('pointerdown', e => {
+            if(brush === 'erase') this.despawn();
+            else if(brush === 'grab') this.drag();
+            else if(brush === 'rope') {
+                if(user.ropeSelection) {
+                    if(!this.roped) user.ropeSelection?.ropeTo(this);
+                    else if(!user.ropeSelection.roped) this.ropeTo(user.ropeSelection());
+                    user.ropeSelection = undefined;
+                }
+                else user.ropeSelection = this;
+            }
+            else if(brush === 'pin') this.pin();
+        });
 
         // Add to list
         entities[entityID] = this;
         entityID++;
+        if(pressed['rclick']) this.drag();
     }
-}
 
-// Planet 1
-// new Planet();
+    get center() {
+        return [ this.s.x+this.s.width/2, this.s.y+this.s.height/2 ]
+    }
+    get speed() {
+        return hypot(this.motion, {x:0,y:0})[0];
+    }
 
-// Tick
-let elapsed = 0.0;
-// let cycle = 0;
+    drag() {
+        this.dragging = true;
+        user.dragging = this;
+    }
+    endDrag() {
+        this.dragging = false;
+        user.dragging = undefined;
+    }
 
-app.ticker.add(gameTick);
-function gameTick(delta) {
-    elapsed += delta;
-    // cycle++;
+    pin(state) {
+        this.pinned = state ?? !this.pinned
+        if(this.pinned) {
+            this.motion = {x:0,y:0};
 
-    // Loop planets
-    for(const [id, pl] of Object.entries(entities)) {
+            this.pinVisual = new PIXI.Graphics();
+            this.pinVisual.beginFill(0xcccccc).drawCircle(0,0, 6, 6).endFill();
+            this.pinVisual.beginFill(0xff6644).drawCircle(0,0, 5, 5).endFill();
+            this.pinVisual.x = this.s.x+this.s.width/2;
+            this.pinVisual.y = this.s.y+this.s.height/2;
+            planetContainer.addChild(this.pinVisual);
+        } else this.removePinVisual();
+    }
+
+    tick() {
+        let speed = this.speed;
+        let strongerFartherFunc = (influencer, distance) => {
+            let pull = Math.pow(influencer.grav_law, distance*2) * influencer.mass * delta;
+            if(pull > 50) pull = 50;
+            return pull;
+        };
+
+        //// GRAVITY
         // Held in place
-        if(pressed['rclick'] && pl.id === entityID-1) accelerate(pl, mouse);
-
+        if(this.dragging) {
+            this.motion.x /= 2;
+            this.motion.y /= 2;
+            this.accelerate(mouse, undefined, strongerFartherFunc);
+            // this.s.x = mouse.s.x;
+            // this.s.y = mouse.s.y;
+        }
         // Loop other planets and have them pull on the current planet
-        else  {
-            for(const [id_influencer, pl_influencer] of Object.entries(entities)) {
-                accelerate(pl, pl_influencer, id, id_influencer);
+        else {
+            // Interactions
+            for(const influencer of Object.values(entities)) {
+                if(this.id === influencer.id) continue;
+
+                // Planet gravity
+                this.accelerate(influencer, config.planetGravityMultiplier);
+
+                // Collision
+                if(!config.do_planet_collision) continue;
+                let [distance, distX, distY] = hypotCenter(influencer.s, this.s);
+                let share = {
+                    x: percentage(distX, distance)/100,
+                    y: percentage(distY, distance)/100,
+                }
+                if(distance < this.s.width) {
+                    this.accelerate(influencer, -10);
+                    influencer.accelerate(this, -10);
+                }
+            }
+        }
+
+        // Ropes
+        if(this.roped) {
+            let ropePullFunc = (influencer, distance) => {
+                let strength = config.stiff_rope ? 0.5 : 0.1;
+                distance -= config.rope_length;
+                let pull = Math.pow(influencer.grav_law, distance*2) * strength * delta;
+                if(pull > 5 && !config.stiff_rope) pull = 5;
+                if(distance <= 0) {
+                    if(config.stiff_rope) pull *= -2;
+                    else return 0;
+                }
+                return pull;
+            };
+            this.accelerate(this.roped, undefined, ropePullFunc);
+            this.roped.accelerate(this, undefined, ropePullFunc);
+
+            this.updateRopeVisual();
+        }
+
+        const actOutCollision = (axis='y', limit, operator='>') => {
+            if(operator === '>' ? this.s[axis] > limit : this.s[axis] < limit) {
+                this.s[axis] = limit - (operator === '>' ? 1 : -1);
+                this.motion[axis] *= -config.wallBounce;
             }
         }
 
         // Bounce off walls
-        // ...
+        if(config.edgeBehavior.startsWith('collide')) {
+            actOutCollision('y', app.view.height-this.s.height, '>'); // Down
+            if(config.edgeBehavior === 'collide') actOutCollision('y', 0, '<'); // Up
+            actOutCollision('x', 0, '<'); // Left
+            actOutCollision('x', app.view.width-this.s.width, '>'); // Right
+        }
+        // Despawn when out of bounds
+        else if(
+            this.s.y < 0-this.s.height || // Down
+            this.s.y > app.view.height+this.s.height || // Up
+            this.s.x < 0-this.s.width || // Left
+            this.s.x > app.view.width+this.s.width // Right
+        ) this.despawn();
 
-        motion(pl);
+        // World gravity
+        if(!this.pinned) {
+            this.motion.y += config.worldGravity * delta;
+            this.motion.x -= this.motion.x * config.airFriction * delta;
+            this.motion.y -= this.motion.y * config.airFriction * delta;
+        }
+        else {
+            this.pinVisual.x = this.s.x+this.s.width/2; this.pinVisual.y = this.s.y+this.s.height/2;
+        }
+        if(!this.pinned || this.dragging) this.runMotion(); // Apply motion
+
+        // Squash and stretch
+        // this.s.scale.y = 1 - (speed/100);
+        // if(this.s.scale.y < 0.5) this.s.scale.y = 0.5;
+        // this.s.angle = this.angle * 180 / Math.PI + 180;
+
+        // Trail
+        if(config.trails) {
+            let alpha = speed/200;
+            new Trail(...this.center, alpha, this.s.width/2);
+        }
     }
 
-    /** Apply Motion */
-    function motion(pl) {
-        pl.s.x += pl.motion.x * delta;
-        pl.s.y += pl.motion.y * delta;
+    // get angle() {
+    //     return (Math.atan2(this.motion.x, this.motion.y) * 360 / Math.PI);
+    // }
+
+    updateRopeVisual() {
+        let distance = hypotCenter(this.s, this.roped.s)[0];
+        let center = this.center;
+        this.ropeVisual.x = center[0]-3;
+        this.ropeVisual.y = center[1]-3;
+        this.ropeVisual.scale.y = distance;
+        this.ropeVisual.angle = -angleRelative(this.s, this.roped.s) * 180 / Math.PI + 180;
     }
 
-    /** Accelerate */
-    function accelerate(pl, pl_influencer, id=-1, id_influencer=-2) {
-        if(id === id_influencer) return;
+    runMotion() {
+        this.s.x += this.motion.x * delta;
+        this.s.y += this.motion.y * delta;
+    }
 
+    ropeTo(subject) {
+        this.roped = subject;
+        this.ropeVisual = new PIXI.Graphics();
+        this.ropeVisual.beginFill(0xffffff).drawRect(0,0, 6, 1).endFill();
+        this.ropeVisual.x = this.s.x;
+        this.ropeVisual.y = this.s.y;
+        planetContainer.addChild(this.ropeVisual);
+
+        this.updateRopeVisual();
+    }
+    detach() {
+        this.ropeVisual.parent.removeChild(this.ropeVisual);
+        this.ropeVisual = undefined;
+    }
+
+    /** Accelerate towards another object */
+    accelerate(influencer, multiplier=1, pullFunc=(influencer, distance) => Math.pow(influencer.grav_law, distance*-1) * influencer.mass * delta) {
+        if(multiplier === 0) return;
         // Accelerate
-        let [distance, distX, distY] = hypot(pl_influencer.s, pl.s);
+        let [distance, distX, distY] = hypotCenter(influencer.s, this.s);
         if(distance === 0) {
             distance = 0.001;
             distX = 0.001;
@@ -96,27 +338,96 @@ function gameTick(delta) {
             y: percentage(distY, distance)/100,
         }
 
+        let pull = pullFunc(influencer, distance);
+        this.motion.x += pull*multiplier*share.x;
+        this.motion.y += pull*multiplier*share.y;
+    }
 
-        let pull = Math.pow(pl_influencer.grav_law, distance*-1) * pl_influencer.mass * delta;
-        pl.motion.x += pull*share.x;
-        pl.motion.y += pull*share.y;
+    removePinVisual() {
+        this.pinVisual.parent.removeChild(this.pinVisual);
+        this.pinVisual = undefined;
+    }
+
+    despawn() {
+        this.s.parent.removeChild(this.s);
+        delete entities[this.id];
+        this.removePinVisual();
+    }
+}
+class Trail {
+    constructor(x=500, y=500, alpha=0.5, size=2, color=0xffffff) {
+        // Sprite
+        this.s = new PIXI.Graphics();
+        this.s.beginFill(color);
+
+        this.s.drawCircle(size, size, size);
+        this.s.endFill();
+        this.s.x = x-this.s.width/2;
+        this.s.y = y-this.s.height/2;
+        this.s.alpha = alpha;
+        this.id = particleID;
+        trailsContainer.addChild(this.s);
+
+        // let time = elapsed;
+        // let expires = elapsed + 10;
+
+        // Add to list
+        particles[particleID] = this;
+        particleID++;
+    }
+
+    tick() {
+        let sw = this.s.width;
+        this.s.width *= 0.95;
+        this.s.height *= 0.95;
+        let off = (sw-this.s.width)/2;
+        this.s.x += off; this.s.y += off;
+        this.s.alpha -= 0.01 * delta;
+        if(this.s.alpha <= 0) this.despawn();
+    }
+
+    despawn() {
+        this.s.parent.removeChild(this.s);
+        delete particles[this.id];
     }
 }
 
+// Planet 1
+// new Planet();
 
-/** Hypotenuese, returns distance between 2 objects */
-function hypot(one, two) {
-    // let distX = one.x+(one.width/2) - two.x+(two.width/2);
-    // let distY = one.y+(one.height/2) - two.y+(two.height/2);
-    let distX = one.x - two.x;
-    let distY = one.y - two.y;
-    return [Math.sqrt(distX**2 + distY**2), distX, distY];
+// Tick
+app.ticker.add(gameTick);
+function gameTick(d) {
+    elapsed += delta;
+    delta = d;
+    delta *= config.game_speed;
+
+    // Loop
+    for(const [id, pl] of Object.entries(entities)) if(!config.pause || pl.dragging) pl.tick(); // Planets
+
+    if(config.pause) return;
+    for(const [id, part] of Object.entries(particles)) part.tick(); // Particles
 }
 
-function percentage(partialValue, totalValue) { return (100 * partialValue) / totalValue; }
-
+function clearScreen() {
+    for(const [id, pl] of Object.entries(entities)) pl.despawn(); // Planets
+    for(const [id, part] of Object.entries(particles)) part.despawn(); // Particles
+}
+function reset() {
+    localStorage.removeItem('gravity_toy_config');
+    location.reload();
+}
 
 // Event Listeners
+// let mouseMotion = {
+//     x:0,
+//     y:0
+// }
+// function average(array) {
+//     return array.reduce((a, b) => a + b, 0) / array.length;
+// }
+// let mouseHistory = [];
+
 const canvas = document.querySelector('canvas');
 canvas.addEventListener('mousemove', event => {
     const rect = canvas.getBoundingClientRect();
@@ -124,17 +435,40 @@ canvas.addEventListener('mousemove', event => {
         event.clientX-rect.left / app.stage.scale.x,
         event.clientY-rect.top / app.stage.scale.y,
     ];
+
+    // mouseHistory.push(mouse.s);
+    // if(mouseHistory.length > 60) mouseHistory.shift();
+    // console.log(average(mouseHistory));
 })
 
-canvas.addEventListener('mousedown', event => {
+canvas.addEventListener('pointerdown', event => {
     pressed['rclick'] = true;
-    let np = new Planet(undefined, mouse.s.x, mouse.s.y, Math.ceil(Math.random() * 16777215));
+    if(brush === 'planet' && !user.dragging) {
+        let off = in_planet_size.value;
+        let np = new Planet(undefined, mouse.s.x-off, mouse.s.y-off, Math.ceil(Math.random() * 16777215));
+    }
 })
-document.addEventListener('mouseup', event => {
+document.addEventListener('pointerup', event => {
     pressed['rclick'] = false;
+    user?.dragging?.endDrag();
 
     document.getElementById('hint').classList.add('fade');
 })
+document.addEventListener('keydown', event => {
+    if(event.key === ' ') event.preventDefault();
+})
+document.addEventListener('keyup', event => {
+    let ae = document.activeElement;
+    if(ae.tagName === 'INPUT' || ae.tagName === 'SELECT') return;
+    document.querySelector(`[data-key="${event.key}"]`)?.click();
+})
+
+const inPlayPause = document.getElementById('play_pause');
+inPlayPause.addEventListener('click', () => {
+    config.pause = !config.pause;
+    inPlayPause.children[0].innerText = config.pause ? '⏵' : '⏸';
+})
+
 
 // Mouse wheel
 document.querySelector('canvas').addEventListener('wheel', event => {
@@ -142,6 +476,6 @@ document.querySelector('canvas').addEventListener('wheel', event => {
 
     let dir = Math.sign(event.deltaY)*-1;
     let multiplier = dir === 1 ? 1.25 : 0.8;
-    app.stage.scale.x *= multiplier;
-    app.stage.scale.y *= multiplier;
+    container.scale.x *= multiplier;
+    container.scale.y *= multiplier;
 })
